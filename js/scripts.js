2f03385d54852bf2173997125df852d591aa885a
Mark Tiapkov <mark.tiapkov@gmail.com>
2020-08-10 16:35:17 -0700

    Adjust prompt wording
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function gallonsToLiters(inputedNumber){
   return inputedNumber * 3.785;
 }
 
-const number1 = parseInt(prompt("Enter a number of gallons you wish to convert to liters"));
+const number1 = parseInt(prompt("Enter a number of gallons you'd like to conver to liters"));
 
 const result = gallonsToLiters(number1);
 alert(result);
